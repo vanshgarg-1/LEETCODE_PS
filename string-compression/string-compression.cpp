08fb435ce461c17c9fd13ee5cbe63485b17a2557
Vansh Garg <124789975+vanshgarg-1@users.noreply.github.com>
2023-05-24 22:30:08 +0530

    Time: 3 ms (90.78%) | Memory: 9 MB (45.61%) - LeetSync

---
+++ string-compression/string-compression.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;
+        int ansindex=0;
+        int n = chars.size();
+
+        while(i<n){
+            
+            int j = i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+
+            chars[ansindex++]=chars[i];
+            int count=j-i;
+
+            if(count>1){
+                string s = to_string(count);
+                for(char ch:s){
+                    chars[ansindex++]= ch ;
+                }
+            }
+            i=j;
+        }
+        return ansindex;
+    }
+};
