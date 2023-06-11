dc8745285ccd00ec6c870094e3bb2866bbdb05cc
Vansh Garg <124789975+vanshgarg-1@users.noreply.github.com>
2023-06-11 21:10:36 +0530

    Time: 19 ms (76.06%) | Memory: 16.8 MB (72.39%) - LeetSync

---
+++ single-number/single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int unique =0;
+        for(int i=0;i<nums.size();i++){
+            unique = unique^nums[i];
+        }
+        return unique;
+    }
+};
