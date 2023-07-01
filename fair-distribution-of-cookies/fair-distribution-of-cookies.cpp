ae4dc5e829d95da82af61b0d0ab3c82f5d5cfba1
Vansh Garg <124789975+vanshgarg-1@users.noreply.github.com>
2023-07-01 21:56:41 +0530

    Time: 2134 ms (21.12%) | Memory: 6.8 MB (98.89%) - LeetSync

---
+++ fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void helper(int ind, const vector<int>& cookies, vector<int>& gotC, int k, int& ans) {
+        int n = cookies.size();
+        if (ind == n) {
+            int currMax = INT_MIN;
+            for (int i = 0; i < k; i++) {
+                currMax = max(currMax, gotC[i]);
+            }
+            ans = min(ans, currMax);
+            return;
+        }
+
+        for (int i = 0; i < k; i++) {
+            gotC[i] += cookies[ind]; 
+            helper(ind + 1, cookies, gotC, k, ans);
+            gotC[i] -= cookies[ind];
+        }
+    }
+
+    int distributeCookies(vector<int>& cookies, int k) {
+        int ans = INT_MAX;
+        vector<int> gotC(k, 0);
+        helper(0, cookies, gotC, k, ans);
+        return ans;
+    }
+};
