f722d7a212bd68b228aa9902d33427b2bb7871ab
Vansh Garg <153756516+vanshgarg-1@users.noreply.github.com>
2024-03-16 22:00:10 +0530

    Time: 3 ms (99.11%) | Memory: 10.5 MB (60.80%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,24 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        ios_base::sync_with_stdio(0);
-        if(!head)return false;
-        ListNode* fast=head;
-        ListNode* slow=head;
-
-        while(fast!=NULL){
-            fast=fast->next;
-            if(fast!=NULL)fast=fast->next;
-            slow=slow->next;
-            if(fast==slow && fast!=NULL)return true;
+    bool hasCycle(ListNode* head) {
+        ListNode *fast = head, *slow = head;
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                return true;
+            }
         }
         return false;
     }
